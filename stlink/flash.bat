b51dc38645810fcd7f7d251d8ff95c62e3e729e8
Frantisek Burian <BuFran@seznam.cz>
2013-09-20 16:17:56 +0200

    Add automatic flasher feature
    Works (tested) with:
    
    STM32F0DISCOVERY
    STM32L1DISCOVERY
    STM32VLDISCOVERY
    STM32F3DISCOVERY
    STM32F4DISCOVERY

--- stlink/flash.bat
+++ stlink/flash.bat
@@ -1,2 +1,45 @@
-..\dfu-util-0.7-binaries\win32-mingw32\dfu-util-static -d 1d50:6017 -a 0  -s 0x08002000:leave -D blackmagic.bin
-pause
+@echo BlackMagic Probe discovery automatic flasher
+@echo "Author: Frantisek Burian <BuFran@seznam.cz>"
+
+@echo **************************************************************************
+@echo Resolder the jumpers from DEFAULT to RESERVED state,
+@echo Connect SWD programmer to the jumper pinheader with name DISCOVERY
+@echo Remove any supply-enabling jumper of the discovery
+@echo .
+
+@pause
+
+@echo **************************************************************************
+@echo * STEP 1/3 Erasing read-out protection ....
+@echo .
+
+@arm-none-eabi-gdb --batch -x black_magic_probe_clearoption.scr blackmagic_dfu.elf
+
+@echo done.
+@echo **************************************************************************
+@echo * Please disconnect and connect power supply to the board and press enter.
+@echo .
+
+@pause
+
+@echo **************************************************************************
+@echo * STEP 2/3 Programming DFU bootloader ...
+@echo .
+
+@arm-none-eabi-gdb --batch -x black_magic_probe_program.scr blackmagic_dfu.elf
+
+@echo done.
+
+@echo **************************************************************************
+@echo * STEP 3/3 Programming BlackMagic firmware ..."
+
+@arm-none-eabi-gdb --batch -x black_magic_probe_program.scr blackmagic.elf 
+
+@echo done.
+
+@echo **************************************************************************
+@echo Disconnect the programmer and push back the jumpers to DISCOVERY header
+@echo Resolder jumpers back to DEFAULT from RESERVED state
+@echo .
+
+@pause
